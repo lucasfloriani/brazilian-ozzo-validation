ee5702db1ef45dd72da42a7a43c4e49d5b139191
lucasfloriani <lucas.floriane@gmail.com>
2019-03-28 11:04:20 -0300

    Add validation for phone or cellphone string

--- phone.go
+++ phone.go
@@ -11,6 +11,8 @@ var (
 	Phone = validation.NewStringRule(isPhone, "must be a valid phone")
 	// CellPhone validate if a string is a brazilian cellphone format or not
 	CellPhone = validation.NewStringRule(isCellPhone, "must be a valid cellphone")
+	// PhoneOrCellPhone validate if a string is a brazilian phone/cellphone format or not
+	PhoneOrCellPhone = validation.NewStringRule(isPhoneOrCellPhone, "must be a valid phone or cellphone")
 )
 
 func isPhone(value string) bool {
@@ -21,4 +23,9 @@ func isPhone(value string) bool {
 func isCellPhone(value string) bool {
 	reCellPhone := regexp.MustCompile(`^\([0-9]{2}\) [0-9]{5}-[0-9]{4}$`)
 	return reCellPhone.MatchString(value)
+}
+
+func isPhoneOrCellPhone(value string) bool {
+	regex := regexp.MustCompile(`^\([0-9]{2}\) [0-9]{4,5}-[0-9]{4}$`)
+	return regex.MatchString(value)
 }
